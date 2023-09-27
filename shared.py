bb12d9de431d6c7a869cea242eb67bed020e57e9
BigBaz <louisvincent@famillecapelli.fr>
2023-09-27 00:08:38 -0400

    [+] One more utils func and rename the old one

--- shared.py
+++ shared.py
@@ -2,6 +2,10 @@ import Gene
 import Label
 
 
-def get_genes_by_class(labels: [Label], genes: [Gene], cls: str) -> [[float]]:
+def get_all_genes_by_class(labels: [Label], genes: [Gene], cls: str) -> [[float]]:
     samples: [str] = Label.get_samples_by_class(labels, cls)
     return Gene.get_genes_by_samples(genes, samples)
+
+def get_one_gene_by_class(labels: [Label], genes: [Gene], cls: str, gene_id: int) -> [float]:
+    genes_by_class: [[float]] = get_all_genes_by_class(labels, genes, cls)
+    return [gene[gene_id] for gene in genes_by_class]
