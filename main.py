9a8d8800bb7ccd9dd335a35bfdfd0324674fee95
BigBaz <louisvincent@famillecapelli.fr>
2023-09-28 11:25:02 -0400

    [~] fix typo

--- main.py
+++ main.py
@@ -35,19 +35,19 @@ if __name__ == '__main__':
     #print(get_inter_class_distance_markdown_table(tuple_class_matrix_list) + "\n")
     #print(get_overlap_markdown_table(tuple_class_matrix_list))
 
-    # class_a: str = "BRCA"
-    # class_b: str = "KIRC"
-    #
-    # matrix_a: [[float]] = get_genes_by_class(labels, genes, class_a)
-    # matrix_b: [[float]] = get_genes_by_class(labels, genes, class_b)
-    #
+    class_a: str = "BRCA"
+    class_b: str = "KIRC"
+    
+    # matrix_a: [[float]] = get_all_genes_by_class(labels, genes, class_a)
+    # matrix_b: [[float]] = get_all_genes_by_class(labels, genes, class_b)
+    
     # intra_class_distance_a: float = get_euclidean_intra_class_distance(matrix_a)
     # intra_class_distance_b: float = get_euclidean_intra_class_distance(matrix_b)
-    #
+    
     # inter_class_distance_a_b: float = get_euclidean_inter_class_distance(matrix_a, matrix_b)
-    #
+    
     # overlap_a_b: float = get_overlap(intra_class_distance_a, intra_class_distance_b, inter_class_distance_a_b)
-    #
+    
     # print("intra_class_distance_a : " + str(intra_class_distance_a))
     # print("intra_class_distance_b : " + str(intra_class_distance_b))
     # print("inter_class_distance_a_b : " + str(inter_class_distance_a_b))
