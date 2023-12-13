d6c30dccd253c37005c04de47f8e59bb386c7807
BigBaz <louisvincent@famillecapelli.fr>
2023-12-13 01:20:50 -0500

    [+] Question 4

--- src/data.py
+++ src/data.py
@@ -23,3 +23,21 @@ def load_ratings_with_genre():
     ratings = pd.read_csv("data/ratings1.csv")
     ratings = ratings.drop("timestamp", axis=1)
     return ratings
+
+def unique_genres():
+    """Return a list of all unique genres in the dataset ('no genres listed' excluded)."""
+    movies = load_movies_with_genre()
+    genres = []
+    for genre in movies["genres"]:
+        for g in genre:
+            if g not in genres:
+                genres.append(g)
+    return genres
+
+def unique_userIds(ratings):
+    """Return a list of all unique userIds in the dataset."""
+    ids = []
+    for id_ in ratings["userId"]:
+        if id_ not in ids:
+            ids.append(id_)
+    return ids
