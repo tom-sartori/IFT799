7d33b602d75e0071e1c5d567ecf6c089697896ee
BigBaz <louisvincent@famillecapelli.fr>
2023-12-14 01:19:11 -0500

    [+] cip

--- src/data.py
+++ src/data.py
@@ -1,3 +1,11 @@
+###
+# |          Nom          | Matricule  |   CIP    |
+# |:---------------------:|:----------:|:--------:|
+# |   Alexandre Theisse   | 23 488 180 | thea1804 |
+# | Louis-Vincent Capelli | 23 211 533 | capl1101 |
+# |      Tom Sartori      | 23 222 497 | sart0701 |
+###
+
 import pandas as pd
 import numpy as np
 
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,11 @@
+###
+# |          Nom          | Matricule  |   CIP    |
+# |:---------------------:|:----------:|:--------:|
+# |   Alexandre Theisse   | 23 488 180 | thea1804 |
+# | Louis-Vincent Capelli | 23 211 533 | capl1101 |
+# |      Tom Sartori      | 23 222 497 | sart0701 |
+###
+
 import src.data as data
 
 from sklearn.cluster import SpectralClustering
